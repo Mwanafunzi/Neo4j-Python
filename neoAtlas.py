88fb0a0afa0f4d6953c65e38e86cdff1acc54308
Mwanafunzi <skemp255@gmail.com>
2017-05-05 18:16:59 +0300

    Add files via upload

---
+++ neoAtlas.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#makes a set of country-sub_region-region nodes in neo4j
+
+import csv
+from neo4j.v1 import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "icecream!"))
+session = driver.session()
+
+
+
+session.run("MATCH (n) DETACH DELETE n")
+
+
+file = open ("world_list.txt","r")
+
+contents = csv.reader(file, delimiter=',', quotechar='"')
+for line in contents:
+    [name,alpha2,alpha3,country_code,iso_3166_2,region,sub_region,region_code,sub_region_code] = line
+    session.run("MERGE (c:Country{name: {name} , alpha2: {alpha2}, alpha3: {alpha3}})"
+                "MERGE (sr:SUB_REGION{name: {sub_region}})"
+                "MERGE (c) -[:IS_IN]->(sr)"
+                "MERGE (r:REGION{name: {region}})"
+                "MERGE (sr) -[:IS_IN]-> (r)"
+                "MERGE (p:PLANET{name: {earth}})"
+                "MERGE (r) -[:IS_IN]-> (p)", name = name, alpha2 = alpha2, alpha3 = alpha3, sub_region =sub_region, region = region, earth = "Earth")
+    
